78667197a5cb5ff3723b0339d21832a9572cc1b4
kabata358 <kabata358@gmail.com>
2022-07-04 21:45:06 +0900

    Added a component

--- swing-test/src/swing_test/Exec.java
+++ swing-test/src/swing_test/Exec.java
@@ -2,9 +2,13 @@ package swing_test;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 public class Exec {
 
@@ -21,15 +25,39 @@ public class Exec {
 		// パネルを作成
 		JPanel p = new JPanel();
 		
+		// ラベルを作成
+		JLabel labelAge = new JLabel("年齢");
+		JLabel labelGender = new JLabel("性別");
+		
+		// テキストフィールドを作成
+		JTextField text = new JTextField(2);
+		String str = text.getText();
+		
+		
 		// ボタンを作成
 		JButton btnSave = new JButton("保存");
 		JButton btnReset = new JButton("リセット");
 		JButton btnCancel = new JButton("キャンセル");
 		
+		// ラジオボタンを作成
+		JRadioButton[] radio = new JRadioButton[2];
+		radio[0] = new JRadioButton("男性");
+		radio[1] = new JRadioButton("女性");
+		
+		ButtonGroup btnGroup = new ButtonGroup();
+		btnGroup.add(radio[0]);
+		btnGroup.add(radio[1]);
+		
 		// パネルにコンポーネントを追加
+		p.add(labelAge);
+		p.add(text);
+		p.add(labelGender);
+		p.add(radio[0]);
+		p.add(radio[1]);
 		p.add(btnSave);
 		p.add(btnReset);
 		p.add(btnCancel);
+
 		
 		// ContentPane にコンポーネントを追加
 		frame.getContentPane().add(p, BorderLayout.SOUTH);
